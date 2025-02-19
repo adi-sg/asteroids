308ef1d785aa58a5a454440a4e5ecbb0bab484ef
adi-sg <adityasuiyer@gmail.com>
2025-02-19 14:29:58 -0800

    added player + movement controls and groups

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-  #i nitialize the game and screen
+  #initialize the game and screen
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
   dt = 0 # delta time, amount of time since the last frame was drawn
 
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  Player.containers = (updatable, drawable)
+
+  player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+  
 # game loop
   while(True):
+
+    # check if the window is closed to terminate program
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+      
     screen.fill(color=(0,0,0))
+
+    updatable.update(dt)
+
+    for draw_sprite in drawable:
+      draw_sprite.draw(screen)
     
     pygame.display.flip()
 
