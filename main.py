5cbe5be3fe025d8ae1973c06c45b17319886855c
adi-sg <adityasuiyer@gmail.com>
2025-02-18 17:03:22 -0800

    restrict game to 60 fps

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import pygame
 from constants import *
 
 def main():
+  #i nitialize the game and screen
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  dt = 0 # delta time, amount of time since the last frame was drawn
 
+# game loop
   while(True):
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -12,6 +16,9 @@ def main():
     screen.fill(color=(0,0,0))
     
     pygame.display.flip()
+
+    time = clock.tick(60)
+    dt = time/1000
 
   # print("Starting asteroids!")
   # print(f"Screen width: {SCREEN_WIDTH}")
